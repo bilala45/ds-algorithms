457edd3243bade182c66575abbff197847ebd546
Bilal Ali <bilalali2@hotmail.com>
2022-03-26 17:35:27 -0400

    create structure for priority queue

---
+++ priorityQueue/PriorityQueue.java
@@ -0,0 +1,27 @@
+public class PriorityQueue {
+
+  public ArrayList<Integer> binHeap;
+
+  public PriorityQueue() {
+    binHeap = new ArrayList<Integer>();
+  }
+
+  // peeks at the lowest value in the PQ
+  public static int find_min() {
+
+  }
+
+  // removes the lowest value from the PQ
+  public static int get_min() {
+
+  }
+
+  // inserts value into PQ
+  public static void insert() {
+
+  }
+
+  public static void main (String[] args) {
+
+  }
+}
