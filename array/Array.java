5ee91c00a1b878763afae8050d0b20fd8d29c0d9
Bilal Ali <bilalali2@hotmail.com>
2022-05-06 18:08:18 -0400

    add method to append and display array values

--- array/Array.java
+++ array/Array.java
@@ -15,16 +15,44 @@ public class Array {
   }
 
   // displays size of array
-  public getSize() {
+  public int getSize() {
     return size;
   }
 
   // displays number of elements in array
-  public getLength() {
+  public int getLength() {
     return length;
+  }
+
+  // append value to array
+  public void append(int appendVal) {
+    if (length < size) {
+      elems[length] = appendVal;
+      length++;
+    } else {
+      System.out.println("Array is full.");
+    }
+  }
+
+  // displays values of array
+  public void display() {
+    if (length != 0) {
+      System.out.print("Elements in array: ");
+      for(int i = 0 ; i < length ; i++) {
+        System.out.print(this.elems[i] + " ");
+      }
+      System.out.println("");
+    } else {
+      System.out.println("Array is empty.");
+    }
   }
 
   public static void main (String[] args) {
     Array arr = new Array(5);
+    System.out.println(arr.getSize());
+    arr.append(9);
+    arr.append(7);
+    arr.display();
+    System.out.println(arr.getLength());
   }
 }
