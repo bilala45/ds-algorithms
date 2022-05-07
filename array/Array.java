1737d047e2f89c7fa6ac43ee127e27ba78c4f16c
Bilal Ali <bilalali2@hotmail.com>
2022-05-07 13:29:16 -0400

    add method to insert/delete from array

--- array/Array.java
+++ array/Array.java
@@ -47,12 +47,41 @@ public class Array {
     }
   }
 
+  // insert into array
+  public void insert(int index, int insertVal) {
+    // check that array isn't full
+    if (length < size) {
+      // iterate backwards through array up to index
+      for (int i = length ; i > index ; i--) {
+        // shift array elements to the right by one
+        this.elems[i] = this.elems[i-1];
+      }
+      // insert insertVal at index
+      this.elems[index] = insertVal;
+      // update length
+      length++;
+    }
+  }
+
+  // delete from array
+  public void delete(int index) {
+    // iterate through array up to index
+    for (int i = index ; i < length ; i++) {
+      // shift array elements to the left by one
+      this.elems[i] = this.elems[i+1];
+    }
+    // update length
+    length--;
+  }
+
   public static void main (String[] args) {
     Array arr = new Array(5);
     System.out.println(arr.getSize());
     arr.append(9);
     arr.append(7);
+    arr.append(5);
     arr.display();
-    System.out.println(arr.getLength());
+    arr.insert(1, 8);
+    arr.display();
   }
 }
