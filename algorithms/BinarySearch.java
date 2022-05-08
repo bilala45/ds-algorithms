420d47cb757da8d0fcadcc2bccff94734c77bb28
Bilal Ali <bilalali2@hotmail.com>
2022-05-07 23:35:32 -0400

    add recursive implementation for binary search algo

--- algorithms/BinarySearch.java
+++ algorithms/BinarySearch.java
@@ -1,19 +1,41 @@
 public class BinarySearch {
 
-  public static int binarySearch(int[] searchArr, int value) {
+  // iterative implementation of binary search algorithm
+  public static int binSearch(int[] searchArr, int value) {
+
+    // set initial start and end pointers
     int start = 0;
     int end = searchArr.length - 1;
 
+    // search concludes when start passes end pointer
     while (start <= end) {
-      // int will truncate so we take the floor of the calculated value
+      // mid is calculated this way to prevent overflow
       int mid = start + (end - start)/2;
-
-      if (searchArr[mid] == value){
+      if (searchArr[mid] == value) {
         return mid;
-      } else if (searchArr[mid] < value) {
-        start = mid + 1;
-      } else if (searchArr[mid] > value) {
+      // restrict search field to first half
+      } else if (value < searchArr[mid]) {
         end = mid - 1;
+      // restrict search field to second half
+      } else {
+        start = mid + 1;
+      }
+    }
+    return -1;
+  }
+
+  // recursive implementation of binary search algorithm
+  public static int rBinSearch(int[] searchArr, int value, int start, int end) {
+    if (start <= end) {
+      int mid = start + (end - start)/2;
+      if (searchArr[mid] == value) {
+        return mid;
+      } else if (value < searchArr[mid]) {
+        // recursive call with new end pointer (restricted to first half)
+        return rBinSearch(searchArr, value, start, mid - 1);
+      } else {
+        // recursive call with new start pointer (restricted to second half)
+        return rBinSearch(searchArr, value, mid + 1, end);
       }
     }
     return -1;
@@ -21,6 +43,11 @@ public class BinarySearch {
 
   public static void main(String[] args) {
     int[] test = {0,2,4,5,6,7,9,10};
-    System.out.println(binarySearch(test, 2));
+    System.out.println(binSearch(test, 7)); // 5
+    System.out.println(binSearch(test, 15)); // -1
+    int start = 0;
+    int end = test.length - 1;
+    System.out.println(rBinSearch(test, 7, start, end)); // 5
+    System.out.println(rBinSearch(test, 15, start, end)); // -1
   }
 }
