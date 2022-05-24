43a0d34beeb53e87424539058b84df43416e0164
Bilal Ali <bilalali2@hotmail.com>
2022-05-24 19:24:20 -0400

    add method to insert node in list with sentinel

--- linkedList/SLListSentinel.java
+++ linkedList/SLListSentinel.java
@@ -38,8 +38,35 @@ public class SLListSentinel {
     System.out.println("null");
   }
 
+  // insert node at specified index
+  // separate condition for inserting at start is not necessary with sentinel
+  public void insert(int insertVal, int index) {
+    Node curr = head;
+
+    // traverse to node right before where the new node will be inserted
+    for (int trackIndex = 0 ; trackIndex < index && curr != null ; trackIndex++) {
+      curr = curr.next;
+    }
+
+    if (curr != null) {
+      // insert node
+      Node insertNode = new Node(insertVal);
+      insertNode.next = curr.next;
+      curr.next = insertNode;
+    } else {
+      System.out.println("Index is out of range of list");
+    }
+  }
+
   public static void main (String[] args) {
     SLListSentinel test = new SLListSentinel();
+    test.display();
+    test.insert(2,0);
+    test.insert(4,1);
+    test.insert(6,2);
+    test.insert(8,3);
+    test.insert(10,4);
+    test.insert(3,5245);
     test.display();
   }
 }
