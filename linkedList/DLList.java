7876be00cdf128f73596f9b2ee541a81695726b7
Bilal Ali <bilalali2@hotmail.com>
2022-05-24 12:59:08 -0400

    add method to insert node in doubly linked list

--- linkedList/DLList.java
+++ linkedList/DLList.java
@@ -24,8 +24,65 @@ public class DLList {
   // constructor
   public DLList() {}
 
+  // insert node at start
+  public void insertStart(int insertVal) {
+    // initialize node to store data passed in
+    Node insertNode = new Node(insertVal);
+    // point prev field of insertNode at null
+    insertNode.prev = null;
 
+    // condition if list is empty
+    if (head == null) {
+      insertNode.next = null;
+      // set head at insertNode
+      head = insertNode;
+    } else {
+      // point next field of insertNode at head
+      insertNode.next = head;
+      // point prev field of head at insertNode
+      head.prev = insertNode;
+      // set head at insertNode
+      head = insertNode;
+    }
+  }
+
+  // insert node at specified index
+  public void insert(int insertVal, int index) {
+    if (index == 0) {
+      this.insertStart(insertVal);
+    } else {
+      // node to traverse list
+      Node trav = head;
+
+      // traverse up to index at which you want to insert
+      for (int trackIndex = 0; trackIndex < index - 1 && trav != null; trackIndex++) {
+        trav = trav.next;
+      }
+
+      // initialize node with insertVal data
+      Node insertNode = new Node(insertVal);
+      // point insertNode at next node
+      insertNode.next = trav.next;
+      // point next node back at insertNode if it's not null
+      // if next node is null, then you're at the end of the list
+      if (trav.next != null) {
+        trav.next.prev = insertNode;
+      }
+
+      // point current node at insertNode and insertNode back at current node
+      trav.next = insertNode;
+      insertNode.prev = trav;
+    }
+  }
+
+  // main method
   public static void main (String[] args) {
     DLList test = new DLList();
+    test.insert(1,0);
+    test.insert(3,1);
+    test.insert(5,2);
+    test.insert(7,3);
+    test.insert(9,4);
+    test.insert(11,5);
   }
 }
