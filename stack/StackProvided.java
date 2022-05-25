7813ff693269e3d6c2bc375635a213c3fac2473b
Bilal Ali <bilalali2@hotmail.com>
2022-05-24 22:08:06 -0400

    reorganize stack and queue directories

---
+++ stack/StackProvided.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class StackProvided {
+
+  public static void main (String[] args) {
+    // Generic is the type of value stored in the stack
+    Stack<Integer> newStack = new Stack<>();
+
+    // adds item to top of stack
+    newStack.push(1);
+    newStack.push(3);
+    newStack.push(5);
+
+    // removes item from top of stack
+    store.pop();
+  }
+}
