4bb2e9f61bf8ccb819927e389454752e67ec2907
Bilal Ali <bilalali2@hotmail.com>
2022-05-25 19:59:25 -0400

    class to implement stack with linked list

---
+++ stack/StackLL.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+
+public class StackLL {
+
+  // create stack in linked list
+  private LinkedList<Integer> stack;
+  // index to track top of stack
+  private int top;
+
+  // constructor
+  public StackLL() {
+    stack = new LinkedList<>;
+    top = -1;
+  }
+
+  // push element onto stack
+
+
+  // pop element off stack
+
+
+  // peek at top of stack
+
+
+  // check if stack is empty
+
+
+  // check if stack is full
+
+
+  // main method
+  public static void main (String[] args) {
+
+  }
+}
