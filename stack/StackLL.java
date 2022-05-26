efe076a8ebf91602cb78ae8e7809c9e1daade496
Bilal Ali <bilalali2@hotmail.com>
2022-05-25 20:21:06 -0400

    add method to pop and peek top element from list stack

--- stack/StackLL.java
+++ stack/StackLL.java
@@ -7,30 +7,41 @@ public class StackLL {
 
   // constructor
   public StackLL() {
-    stack = new LinkedList<>;
-    top = -1;
+    stack = new LinkedList<>();
+    // add a sentinel node containing null at the end of the list
+    // push and pop all elements ahead of the sentinel node
+    stack.add(null);
   }
 
   // push element onto stack
   public void push(int pushElem) {
-    // append pushElem to start of list
+    // append pushElem to head of list
     stack.addFirst(pushElem);
   }
 
   // pop element off stack
-
+  public int pop() {
+    return stack.remove();
+  }
 
   // peek at top of stack
-
+  public int peek() {
+    // retrieves head element of list
+    return stack.getFirst();
+  }
 
   // check if stack is empty
-
 
   // check if stack is full
 
 
   // main method
   public static void main (String[] args) {
-
+    StackLL test = new StackLL();
+    test.push(3);
+    test.push(5);
+    test.push(7);
+    System.out.println(test.pop()); // 7
+    System.out.println(test.peek()); // 5
   }
 }
