55920f520b4e5b5ffbf71706406db21ce4a5c4ff
Bilal Ali <bilalali2@hotmail.com>
2022-06-03 12:47:55 -0400

    class to implement double ended queue (deque)

---
+++ queue/Deque.java
@@ -0,0 +1,12 @@
+public class Deque {
+
+  // int array to store deque
+  private int[] deque;
+  // front and back index pointers
+  private int front;
+  private int back;
+
+  public static void main(String[] args) {
+
+  }
+}
