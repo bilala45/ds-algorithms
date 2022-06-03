7c9a350855f7e90412c96026e8fe59b795e1efda
Bilal Ali <bilalali2@hotmail.com>
2022-06-03 19:26:49 -0400

    add method to dequeue from queue implemented with 2 stacks

--- queue/Queue2Stack.java
+++ queue/Queue2Stack.java
@@ -20,11 +20,30 @@ public class Queue2Stack {
 
   // dequeue value from queue
   public Integer dequeue() {
-
+    // handles empty queue
+    if (deq.empty() && enq.empty()) {
+      System.out.println("Queue is empty");
+      return null;
+    // pop from enq stack and push into deq stack until enq stack is empty
+    } else if (deq.empty()) {
+      // iterate through enq stack until empty
+      while (!enq.empty()) {
+        // pop from enq stack and push into deq stack
+        deq.push(enq.pop());
+      }
+    }
+    // pop and return top value of deq stack
+    return deq.pop();
   }
 
   // main method
   public static void main(String[] args) {
-
+    Queue2Stack test = new Queue2Stack();
+    test.enqueue(6);
+    test.enqueue(4);
+    test.enqueue(2);
+    System.out.println(test.dequeue());
+    System.out.println(test.dequeue());
+    System.out.println(test.dequeue());
   }
 }
