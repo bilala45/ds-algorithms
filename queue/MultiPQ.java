abbf33277ab9c4a124575250cbdb061e5064870a
Bilal Ali <bilalali2@hotmail.com>
2022-06-03 18:04:12 -0400

    add method to insert into priority queue

--- queue/MultiPQ.java
+++ queue/MultiPQ.java
@@ -14,7 +14,51 @@ public class MultiPQ {
     front1 = back1 = front2 = back2 = -1;
   }
 
-  public static void main (String[] args) {
+  // insert into priority queue
+  public void enqueue(int val, int priority) {
+    // insert item in queue corresponding to priority
+    if (priority == 1) {
+      // handles queue with no inserted values yet
+      if (front1 == -1) {
+        // update front and back pointers
+        front1 = back1 = 0;
+        pq1[front1] = val;
+      // handles full queue
+    } else if (back1 == pq1.length - 1) {
+        System.out.println("Queue 1 is full");
+      // insert into queue
+      } else {
+        back1 += 1;
+        pq1[back1] = val;
+      }
+    } else if (priority == 2) {
+      // handles queue with no inserted values yet
+      if (front2 == -1) {
+        // update front and back pointers
+        front2 = back2 = 0;
+        pq2[front2] = val;
+      // handles full queue
+    } else if (back2 == pq2.length - 1) {
+        System.out.println("Queue 2 is full");
+      // insert into queue
+      } else {
+        back2 += 1;
+        pq2[back2] = val;
+      }
+    } else {
+      System.out.println("Invalid priority");
+    }
+  }
 
+  public static void main (String[] args) {
+    MultiPQ test = new MultiPQ();
+    test.enqueue(1,1);
+    test.enqueue(3,1);
+    test.enqueue(5,1);
+    test.enqueue(7,1);
+    test.enqueue(2,2);
+    test.enqueue(4,2);
+    test.enqueue(9,1);
+    test.enqueue(11,1);
   }
 }
