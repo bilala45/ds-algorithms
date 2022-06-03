bf7e3182efe582ccaeb11fd407072db9d350ce6c
Bilal Ali <bilalali2@hotmail.com>
2022-06-03 12:50:51 -0400

    add constructor to initialize deque

--- queue/Deque.java
+++ queue/Deque.java
@@ -6,6 +6,15 @@ public class Deque {
   private int front;
   private int back;
 
+  // constructor
+  public Deque() {
+    // initialize array to hold deque
+    deque = new int[10];
+    // initialize front and back at -1 to indicate empty deque
+    front = -1;
+    back = -1;
+  }
+
   public static void main(String[] args) {
 
   }
