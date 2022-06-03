534c0038b167ba4cc30a7a98c918862efc30686f
Bilal Ali <bilalali2@hotmail.com>
2022-06-03 19:13:33 -0400

    class for queue using 2 stacks

---
+++ queue/Queue2Stack.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Queue2Stack {
+
+  // stack fields for enqueue and dequeue
+  private Stack<Integer> enq;
+  private Stack<Integer> deq;
+
+  public Queue2Stack() {
+    // initialize enq and deq stacks
+    enq = new Stack<Integer>();
+    deq = new Stack<Integer>();
+  }
+
+  // insert value in queue
+  public void enqueue(int val) {
+
+  }
+
+  // dequeue value from queue
+  public Integer dequeue() {
+
+  }
+
+  // main method
+  public static void main(String[] args) {
+
+  }
+}
