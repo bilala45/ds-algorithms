6f43bd73cdc0aa121687dd01717c34b97f2b3bce
Bilal Ali <bilalali2@hotmail.com>
2022-06-05 18:16:44 -0400

    add private node class for nodes in linked list binary tree

--- binaryTree/BinTreeLL.java
+++ binaryTree/BinTreeLL.java
@@ -3,6 +3,29 @@ public class BinTreeLL {
   // root node
   private Node root;
 
+  // node class
+  private class Node {
+    // node data
+    private int data;
+    // left and right child
+    private Node left;
+    private Node right;
+
+    // left and right fields are automatically set to null if values aren't provided in constructor
+    // Node constructor (null)
+    public Node() {}
+
+    // Node constructor (data)
+    public Node(int data) {
+      // assign data argument to Node's data field
+      this.data = data;
+    }
+  }
+
+  // binary tree constructor
+  // initializes binary tree with root set to null
+  public BinTreeLL() {}
+
   // main method
   public static void main(String[] args) {
 
