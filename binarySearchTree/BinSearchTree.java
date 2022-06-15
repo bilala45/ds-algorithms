f4fb68af63674e7ab8d4f7b7f1072d0ad3489ce2
Bilal Ali <bilalali2@hotmail.com>
2022-06-15 14:29:59 -0400

    add methods for iter and recur search in a BST

--- binarySearchTree/BinSearchTree.java
+++ binarySearchTree/BinSearchTree.java
@@ -29,9 +29,44 @@ public class BinSearchTree {
 
   }
 
-  // search in BST
-  public static void searchBST(int key) {
+  // iterative search in BST
+  public static boolean iterSearchBST(BinSearchTree tree, int key) {
+    // check that tree contains values
+    if (tree.root != null) {
+      // initialize curr pointer at tree root
+      Node curr = tree.root;
 
+      // traverse tree levels until leaf node is reached
+      while (curr != null) {
+        // break loop if key matches data at current node
+        if (curr.data == key) {
+          return true;
+        // move to right subtree if key is greater than data at current node
+        } else if (key > curr.data) {
+          curr = curr.right;
+        // move to left subtree if key is less than data at current node
+        } else {
+          curr = curr.left;
+        }
+      }
+    }
+    return false;
+  }
+
+  // recursive search in BST
+  public static boolean recurSearchBST(Node currNode, int key) {
+    // check for null node
+    if (currNode == null) {
+      return false;
+    }
+    // compare key to data in currNode
+    if (currNode.data == key) {
+      return true;
+    } else if (key > currNode.data) {
+      recurSearchBST(currNode.right, key);
+    } else {
+      recurSearchBST(currNode.left, key);
+    }
   }
 
   // main method
