43ba885d32b261439f6e6ef506255b24f6b98e15
Bilal Ali <bilalali2@hotmail.com>
2022-06-15 14:15:37 -0400

    class for binary search tree implementation

---
+++ binarySearchTree/BinSearchTree.java
@@ -0,0 +1,41 @@
+public class BinSearchTree {
+
+  // root node
+  private Node root;
+
+  // node class
+  private class Node {
+
+    // data field
+    private int data;
+    // child nodes
+    private Node left;
+    private Node right;
+
+    // empty constructor
+    private BinSearchTree() {}
+
+    // constructor with data passed in
+    private BinSearchTree(int data) {
+      this.data = data;
+    }
+  }
+
+  // constructor
+  public BinSearchTree() {}
+
+  // insert node in BST
+  public void insertNode(int nodeData) {
+
+  }
+
+  // search in BST
+  public static void searchBST(int key) {
+
+  }
+
+  // main method
+  public static void main(String[] args) {
+
+  }
+}
