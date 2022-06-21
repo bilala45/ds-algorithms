c0c36cb359013dda2f1b8f264f07a32e4b5e7074
Bilal Ali <bilalali2@hotmail.com>
2022-06-20 20:54:45 -0400

    add method for insertion sort

--- sorts/Sorts.java
+++ sorts/Sorts.java
@@ -1,28 +1,60 @@
 public class Sorts {
 
   // bubble sort
-  public static int[] bubbleSort(int[] sortArr) {
+  public static int[] bubbleSort(int[] arr) {
     // on each iteration, the largest element in the array is moved to the end
     // decrement the size of the array (from the right size) on each iteration
-    for (int i = sortArr.length - 1 ; i > 0 ; i--) {
-      // iterate up to i
+    for (int i = arr.length - 1 ; i > 0 ; i--) {
+      // iterate up to i (not including i because we don't need to compare the last value with anything)
       for (int j = 0 ; j < i ; j++) {
-        if (sortArr[j] > sortArr[j+1]) {
-          int temp = sortArr[j];
-          sortArr[j] = sortArr[j+1];
-          sortArr[j+1] = temp;
+        // swap array elements if element at j is greater than element at j+1
+        if (arr[j] > arr[j+1]) {
+          // swap
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
         }
       }
+      display(arr);
     }
-    return sortArr;
+    return arr;
+  }
+
+  // insertion sort
+  public static int[] insertionSort(int[] arr) {
+    // start at index 1 (beginning of unsorted group)
+    // the first element (index 0) is already "sorted" since it's a single element
+    for (int i = 1 ; i < arr.length ; i++) {
+      // store element at start of unsorted group
+      int curr = arr[i];
+      int sortedElem = i - 1;
+
+      // shift while element at j (from sorted group) is greater than curr
+      while (sortedElem != -1 && arr[sortedElem] > curr) {
+        arr[sortedElem+1] = arr[sortedElem];
+        // decrement j on each iteration
+        sortedElem--;
+      }
+      // update next element with curr
+      arr[sortedElem+1] = curr;
+      display(arr);
+    }
+    return arr;
+  }
+
+  // display elements of array
+  private static void display(int[] arr) {
+    System.out.print("[");
+    for (int i = 0 ; i < arr.length - 1 ; i++) {
+      System.out.print(arr[i] + ",");
+    }
+    System.out.println(arr[arr.length - 1] + "]");
   }
 
   // main method
   public static void main(String[] args) {
-    int[] unsorted = {3,6,2,1,7,4};
-    int[] bubble = bubbleSort(unsorted);
-    for (int i = 0 ; i < bubble.length ; i++) {
-      System.out.println(bubble[i]);
-    } 
+    int[] unsorted = {3,6,4,2,1};
+    //int[] bubble = bubbleSort(unsorted);
+    int[] insertion = insertionSort(unsorted);
   }
 }
