3bd7a467a081fbad2305d9b65c3fb05eab45c213
Bilal Ali <bilalal495@outlook.com>
2022-06-26 21:52:19 -0400

    create class for hash map implementation

---
+++ hashMap/HashMap.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class HashMap {
+
+  // array of linked lists to store data in hash map
+  private LinkedList<Integer>[] hashMap;
+
+  // constructor
+  public HashMap() {
+    // initialize hash map of size 10
+    hashTable = new LinkedList<Integer>[10];
+  }
+
+  // hash function
+  private static int hashFunction(int key) {
+
+  }
+
+  // insert value in hash map
+  public static void insert(int value) {
+
+  }
+
+  // main method
+  public static void main(String[] args) {
+
+  }
+}
