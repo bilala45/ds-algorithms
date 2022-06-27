32d8776ea6e40c6ab449fb472efcd47c15810973
Bilal Ali <bilalal495@outlook.com>
2022-06-26 21:59:30 -0400

    add method to insert in hash map with closed addressing

--- hashMap/HashMap.java
+++ hashMap/HashMap.java
@@ -8,21 +8,33 @@ public class HashMap {
   // constructor
   public HashMap() {
     // initialize hash map of size 10
-    hashTable = new LinkedList<Integer>[10];
+    hashMap = new LinkedList[10];
   }
 
   // hash function
   private static int hashFunction(int key) {
-
+    // mod key with 10 and return result
+    return key%10;
   }
 
   // insert value in hash map
-  public static void insert(int value) {
+  public void insert(int key) {
+    // hash key to get mapping index
+    int index = hashFunction(key);
 
+    // handle collisions with chaining
+    if (hashMap[index] == null) {
+      hashMap[index] = new LinkedList<Integer>();
+    }
+    // insert key at index
+    hashMap[index].add(key);
   }
 
   // main method
   public static void main(String[] args) {
-
+    HashMap test = new HashMap();
+    test.insert(5);
+    test.insert(28);
+    test.insert(13);
   }
 }
