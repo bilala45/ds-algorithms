6168aa93cd1ff13fb79dde536188827c127add31
Bilal Ali <bilalal495@outlook.com>
2022-06-28 19:50:06 -0400

    add method for breadth first search in undirected graph

--- graph/UndirectedGraph.java
+++ graph/UndirectedGraph.java
@@ -1,3 +1,6 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class UndirectedGraph {
 
   // adjacency matrix to store graph
@@ -34,9 +37,12 @@ public class UndirectedGraph {
     }
     System.out.println("");
 
+    // graph.length gives the number of rows
     for (int i = 0 ; i < graph.length ; i++) {
       System.out.print(i + " ");
-      for (int j = 0 ; j < graph.length ; j++) {
+
+      // graph[i].length gives the number of columns in that row
+      for (int j = 0 ; j < graph[i].length ; j++) {
         System.out.print(graph[i][j] + " ");
       }
       System.out.println("");
@@ -45,14 +51,54 @@ public class UndirectedGraph {
   }
 
   // breadth first search
+  public static void bfs(UndirectedGraph udGraph, int start) {
+    // start is the starting vertex for our bfs search
 
+    // reference to graph field in udGraph
+    int[][] graph = udGraph.graph;
+    // initialize visited array to track vertices that have already been visited (and printed to the screen)
+    int[] visited = new int[graph.length];
+    // initialize queue to store adjacent vertices of current vertex
+    Queue<Integer> toVisit = new LinkedList<Integer>();
+
+    // add start to toVisit queue
+    toVisit.offer(start);
+    // print start and update value in visited list
+    System.out.print(start + " ");
+    visited[start] = start;
+
+    // iterate through queue as long as queue is not empty
+    while (toVisit.peek() != null) {
+      // dequeue from front of queue and store in curr
+      int curr = toVisit.poll();
+
+      // iterate through row in adjacency matrix
+      for (int i = 0 ; i < udGraph.vertices ; i++) {
+        // enqueue, print, and update non-zero values in visited list
+        if (graph[curr][i] != 0 && visited[i] != i) {
+          // add start to toVisit queue
+          toVisit.offer(i);
+          // print start and update value in visited list
+          System.out.print(i + " ");
+          visited[i] = i;
+        }
+      }
+    }
+    System.out.println("");
+  }
 
   // main method
   public static void main(String[] args) {
     UndirectedGraph test = new UndirectedGraph(5);
     display(test);
-    int[] adj1 = {2, 4};
+    int[] adj1 = {2, 3};
     test.insert(1, adj1);
+    int[] adj2 = {1, 4};
+    test.insert(2, adj2);
+    int[] adj3 = {1, 4};
+    test.insert(3, adj3);
     display(test);
+
+    bfs(test, 4);
   }
 }
