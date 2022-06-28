639353b04bfe33ddf0ec12aff7cf4938f6e8d881
Bilal Ali <bilalal495@outlook.com>
2022-06-28 17:40:45 -0400

    add class for undirected graph

---
+++ graph/UndirectedGraph.java
@@ -0,0 +1,19 @@
+public class UndirectedGraph {
+
+  // adjacency matrix to store graph
+  private int[][] graph;
+
+  // constructor
+  public UndirectedGraph() {
+    // initialize matrix of size 10x10
+    graph = new int[10][10];
+  }
+
+  // breadth first searchj
+
+
+  // main method
+  public static void main(String[] args) {
+
+  }
+}
