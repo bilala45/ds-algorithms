5faf869be9f1c4ac5b35d11d071b1141f755aaaf
Bilal Ali <bilalal495@outlook.com>
2022-07-19 11:48:14 -0400

    create class for fibonacci dp exercises

---
+++ dynamicProgramming/FibonacciNums.java
@@ -0,0 +1,24 @@
+public class FibonacciNums {
+
+  // naive approach
+  public static int fibNaive(int num) {
+
+  }
+
+
+  // top-down (memoization)
+  public static int fibMemo(int num) {
+
+  }
+
+
+  // bottom-up (tabulation)
+  public static int fibTab(int num) {
+
+  }
+
+
+  public static void main(String[] args) {
+
+  }
+}
