beca4748c13d8e67557c37d11e03a7fc062fc6cf
Bilal Ali <bilalal495@outlook.com>
2022-07-20 01:01:06 -0400

    Add method to compute nth fibonacci number using bottom up dp approach

--- dynamicProgramming/FibonacciNums.java
+++ dynamicProgramming/FibonacciNums.java
@@ -6,9 +6,7 @@ public class FibonacciNums {
   // naive approach
   public static int fibNaive(int num) {
     // base cases (num = 0, 1)
-    if (num <= 1) {
-      return num;
-    }
+    if (num <= 1) return num;
 
     // sum previous two numbers
     return fibNaive(num - 1) + fibNaive(num - 2);
@@ -23,13 +21,9 @@ public class FibonacciNums {
   // top-down approach helper method
   private static int fibMemoHelper(int num, Map<Integer, Integer> memo) {
     // check if input has been cached
-    if (memo.get(num) != null) {
-      return memo.get(num);
-    }
+    if (memo.get(num) != null) return memo.get(num);
     // base cases (num = 0, 1)
-    if (num <= 1) {
-      return num;
-    }
+    if (num <= 1) return num;
 
     // cache result
     memo.put(num, fibMemoHelper(num - 1, memo) + fibMemoHelper(num - 2, memo));
@@ -38,15 +32,30 @@ public class FibonacciNums {
 
   // bottom-up (tabulation)
   public static int fibTab(int num) {
-    return 0;
+    // array indexing begins at 0
+    int[] tab = new int[num + 1];
+
+    // base cases
+    tab[0] = 0;
+    tab[1] = 1;
+
+    // iterate through tab array
+    for (int i = 0 ; i < tab.length ; i++) {
+      // update next two positions with value at current index
+      if (i + 1 < tab.length) {
+        tab[i+1] += tab[i];
+      }
+      if (i + 2 < tab.length) {
+        tab[i+2] += tab[i];
+      }
+    }
+
+    return tab[num];
   }
 
-
   public static void main(String[] args) {
-    System.out.println(fibNaive(6)); // 8
-    System.out.println(fibNaive(40)); // 102334155
-
-    System.out.println(fibMemo(6)); // 8
-    System.out.println(fibMemo(40)); // 102334155
+    System.out.println(fibTab(3)); // 2
+    System.out.println(fibTab(20)); // 6765
+    System.out.println(fibTab(40)); // 102334155
   }
 }
